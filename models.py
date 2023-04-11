378e891298a671704d2c3b11f299f789ecf0bcf5
zuxriddin99 <zux.828@gmail.com>
2023-04-12 01:59:29 +0500

    add sub category model

--- models.py
+++ models.py
@@ -4,7 +4,8 @@ from admin import db
 class Categories(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    # parent_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    parent_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    parent = db.relationship('Categories', remote_side=[id])
 
     def __repr__(self):
         return self.name
@@ -12,30 +13,29 @@ class Categories(db.Model):
 
 class Medicines(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True,nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'),nullable=False)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
     categories = db.relationship('Categories')
-    country = db.Column(db.String(40),nullable=False)
+    country = db.Column(db.String(40), nullable=False)
     active_substance_id = db.Column(db.Integer, db.ForeignKey('active_substances.id'))
     active_substance = db.relationship('Active_substances')
     representative = db.Column(db.String(50))
     manufacturer_id = db.Column(db.Integer, db.ForeignKey('manufacturers.id'))
     manufacturer = db.relationship('Manufacturers')
     code_atx = db.Column(db.String(50))
-    price = db.Column(db.Float,nullable=False)
+    price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text)
     image_url = db.Column(db.String(255), nullable=True, default='default.jpg')
 
 
-
 class Manufacturers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50),nullable=False)
+    name = db.Column(db.String(50), nullable=False)
 
 
 class Active_substances(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50),nullable=False)
+    name = db.Column(db.String(50), nullable=False)
 
 
 class Cart_items(db.Model):
@@ -65,7 +65,7 @@ class Order_items(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True,nullable=False)
-    email = db.Column(db.String(64), unique=True,nullable=False)
-    phone = db.Column(db.String(64), unique=True,nullable=False)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(64), unique=True, nullable=False)
+    phone = db.Column(db.String(64), unique=True, nullable=False)
     password = db.Column(db.String(128))
